414b6ec25938596280c5587609d56ee18977bd65
ilkuzmenko <illia.kuzmenko@icloud.com>
2021-08-17 12:56:17 +0300

    Fix output

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -7,4 +7,4 @@ from loader import dp
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer("Виберіть, будь ласка, зі списку", reply_markup=menu)
+    await message.answer(f"Hi, {message.from_user.full_name} is start command!", reply_markup=menu)
